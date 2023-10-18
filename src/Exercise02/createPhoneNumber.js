e525227f43b91d38d760d9afc41dde3610ee1f33
Bateyjosue <josuebatey19@gmail.com>
2023-10-18 11:35:50 +0200

    refactor: create phone number

--- src/Exercise02/createPhoneNumber.js
+++ src/Exercise02/createPhoneNumber.js
@@ -1,4 +1,7 @@
 const createPhoneNumber = (numbers) => {
-    numbers = numbers.join('');
-    return `(${numbers.substring(0, 3)}) ${numbers.substring(3, 6)}-${numbers.substring(6)}`
-}
+  numbers = numbers.join("");
+  return `(${numbers.substring(0, 3)}) ${numbers.substring(
+    3,
+    6
+  )}-${numbers.substring(6)}`;
+};
