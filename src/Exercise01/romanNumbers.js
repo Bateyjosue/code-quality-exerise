645d531e208649fd5543f5cd591732361bbd1574
Bateyjosue <josuebatey19@gmail.com>
2023-10-19 11:13:28 +0200

    refactor: decoder and encoder function

--- src/Exercise01/romanNumbers.js
+++ src/Exercise01/romanNumbers.js
@@ -1,14 +1,14 @@
-function romanNumeralsDecoder(roman) {
-  const romanNumeralsMap = new Map([
-    ["I", 1],
-    ["V", 5],
-    ["X", 10],
-    ["L", 50],
-    ["C", 100],
-    ["D", 500],
-    ["M", 1000],
-  ]);
+const romanNumeralsMap = new Map([
+  ["I", 1],
+  ["V", 5],
+  ["X", 10],
+  ["L", 50],
+  ["C", 100],
+  ["D", 500],
+  ["M", 1000],
+]);
 
+function romanNumeralsDecoder(roman) {
   /*
         After converting the roman to numeral. 
         sum up the numerals using reducer function
@@ -24,23 +24,23 @@ function romanNumeralsDecoder(roman) {
     );
 }
 
-const romanNumeralsEncoder = (number) => {
-  const romanNumerals = [
-    { value: 1000, numeral: "M" },
-    { value: 900, numeral: "CM" },
-    { value: 500, numeral: "D" },
-    { value: 400, numeral: "CD" },
-    { value: 100, numeral: "C" },
-    { value: 90, numeral: "XC" },
-    { value: 50, numeral: "L" },
-    { value: 40, numeral: "XL" },
-    { value: 10, numeral: "X" },
-    { value: 9, numeral: "IX" },
-    { value: 5, numeral: "V" },
-    { value: 4, numeral: "IV" },
-    { value: 1, numeral: "I" },
-  ];
+const romanNumerals = [
+  { value: 1000, numeral: "M" },
+  { value: 900, numeral: "CM" },
+  { value: 500, numeral: "D" },
+  { value: 400, numeral: "CD" },
+  { value: 100, numeral: "C" },
+  { value: 90, numeral: "XC" },
+  { value: 50, numeral: "L" },
+  { value: 40, numeral: "XL" },
+  { value: 10, numeral: "X" },
+  { value: 9, numeral: "IX" },
+  { value: 5, numeral: "V" },
+  { value: 4, numeral: "IV" },
+  { value: 1, numeral: "I" },
+];
 
+const romanNumeralsEncoder = (number) => {
   let combinedRomans = "";
   romanNumerals.forEach(({ value, numeral }) => {
     while (number >= value) {
