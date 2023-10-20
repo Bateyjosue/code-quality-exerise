af837c84de065fbb0aa84c32b50f6b5b3796debd
Bateyjosue <josuebatey19@gmail.com>
2023-10-20 13:15:25 +0200

    refactor: calculate factorial using recursion

--- src/Exercise03/calculateFactorial.js
+++ src/Exercise03/calculateFactorial.js
@@ -4,3 +4,14 @@ const calculateFactorial = (n) => {
       (prod, currentNumber) => prod * currentNumber
     , 1);
 }
+
+const calculateFactorialRecursion = (n) => {
+  // base condition
+  if(n < 2) return 1
+  /*
+    call itself until the base is met
+  */
+  return n * calculateFactorialRecursion(n - 1)
+};
+
+console.log(calculateFactorialRecursion(5));
