7d2c4ad8eaf3a9f3895594019283d7c6f7779ffe
Bateyjosue <josuebatey19@gmail.com>
2023-10-27 11:30:11 +0200

    fix: aply review changes on calculate fibonacci

--- src/Exercise05/calculate-fibonacci.js
+++ src/Exercise05/calculate-fibonacci.js
@@ -3,7 +3,7 @@ const calculateFibonacci = (number) => {
     if (number === 1) return [0];
     if (number === 2) return [0, 1];
 
-    const fibonaccis = calculateFibonacci(number - 1);
-    fibonaccis.push(fibonaccis.at(-1) + fibonaccis.at(-2));
-    return fibonaccis;
+    const fibonacciSeries = calculateFibonacci(number - 1);
+    fibonacciSeries.push(fibonacciSeries.at(-1) + fibonacciSeries.at(-2));
+    return fibonacciSeries;
 }
